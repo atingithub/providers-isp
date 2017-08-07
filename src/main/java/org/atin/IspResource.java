2ab2fbd87f6baaf59385949d2172ebdeb4da907b
Atin Hansda <atin.hansda@gmail.com>
2017-08-08 01:08:21 +0530

    fixed build errors

--- src/main/java/org/atin/IspResource.java
+++ src/main/java/org/atin/IspResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
@@ -38,7 +37,8 @@ public class IspResource {
     @GET  
     @Path("/download")  
     @Produces("application/pdf")  
-    public Response downloadFile(@QueryParam("filename") String fileName,
+    public Response downloadFile(
+    		@QueryParam("filename") String fileName,
     		@Context ServletContext servletContext) {
     	
     	String downloadFilePath =
